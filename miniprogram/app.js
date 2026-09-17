930f65c18c1f53159266a5e6c24fe423fc67c152
caix <cai1058@qq.com>
2026-09-17 21:34:47 +0800

    配置修改

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -9,32 +9,32 @@ App({
         isRemoved: true, // 手动党（为防止加载初始项目时因为没有云开发环境而报错，我先设为true，等搞好云开发环境后再把它改回false）
 
         // 魔法开关，开启后可使用完整功能，包括填写表单、祝福语轮播和视频号播放器等等
-        // magic: new Date() * 1 >= 1699401600000, // 自动党，用指定时间戳来控制自动开启
-        magic: true, // 手动党（方便预览完整功能，我先设为true）
+        magic: new Date() * 1 >= 1699401600000, // 自动党，用指定时间戳来控制自动开启
+        // magic: true, // 手动党（方便预览完整功能，我先设为true）
 
         // 婚礼日期时间
-        weddingTime: '2023-09-20 17:00:00',
+        weddingTime: '2026-11-18 17:00:00',
 
         // 新郎新娘信息
         couple: [{
             image: 'https://res.wx.qq.com/t/fed_upload/52557f40-32f9-4b37-9115-eed527e55683/husband.jpg', // 新郎单人照
-            name: '周杰伦', // 姓名
+            name: '蔡鑫', // 姓名
             alias: '新郎', // 称谓
-            number: 'XXXXXXXXXXX', // 手机号码
-            birthday: '1979.01.18' // 出生日期
+            number: '13670401092', // 手机号码
+            birthday: '1995.08.21' // 出生日期
         }, {
             image: 'https://res.wx.qq.com/t/fed_upload/e4ddf6ce-990c-45b1-b4e9-bdf6a6b498ac/wife.jpg', // 新娘单人照
-            name: '昆凌', // 姓名
+            name: '何洁琼', // 姓名
             alias: '新娘', // 称谓
-            number: 'XXXXXXXXXXX', // 手机号码
-            birthday: '1993.08.12' // 出生日期
+            number: '13169642658', // 手机号码
+            birthday: '1994.07.08' // 出生日期
         }],
 
         // 发布者（自己想个你俩人的噱头组合名呗）
-        publisher: '杰伦昆凌',
+        publisher: '蔡鑫洁琼',
 
         // 纪念日（如果是一见钟情的话，建议用第一次见面那天）
-        anniversary: '2021.12.11'
+        anniversary: '2024.01.07'
     },
 
     // 小程序启动时，初始化云开发环境
